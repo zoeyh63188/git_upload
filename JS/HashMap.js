f9d9664f45ff25947d044d6e1ffb253c0cebb6ed
Zoey_00550347 <zoeyhuangy.c@gmail.com>
2024-08-21 17:11:21 +0800

    JS 評量 CRed

--- JS/HashMap.js
+++ JS/HashMap.js
@@ -1,17 +1,17 @@
 var HashMap = function () {
   let map = {};
   return {
-    put: function (k, v) {
-      map[k] = v;
+    put: function (key, value) {
+      map[key] = value;
     },
     keys: function () {
       return Object.keys(map);
     },
-    contains: function (k) {
-      return k in map;
+    contains: function (key) {
+      return key in map;
     },
-    get: function (k) {
-      return map[k];
+    get: function (key) {
+      return map[key];
     },
     clear: function () {
       return (map = {});
@@ -19,32 +19,5 @@ var HashMap = function () {
   };
 };
 
-let hashMap = new HashMap();
-let resultStr = '';
-document.getElementById("buttonPut").addEventListener('click', function () {
-  let key = document.getElementById('inputBoxKey').value;                
-  let value = document.getElementById('inputBoxValue').value;
- 
-  console.log("Key:", key); // Debugging output
-  console.log("Value:", value); // Debugging output
-
-  if (key === null || key === "") {
-    alert("請輸入KEY");
-
-    return;
-  } else if (hashMap.contains(key)) {
-    alert("KEY已存在!");
-    return;
-  } else {
-    hashMap.put(key, value);
-    resultStr += (key + " : " + hashMap.get(key) + '<br>');
-    document.getElementById('resultContent').innerHTML = resultStr;
-  }
-});
 
 
-document.getElementById('buttonClear').addEventListener('click', function(){
-    resultStr = '';
-    document.getElementById('resultContent').innerHTML = resultStr;
-    hashMap.clear();
-})
--- JS/Table.js
+++ JS/Table.js
@@ -10,57 +10,56 @@ document.getElementById('btnInsert').addEventListener('click', function () {
   } else {
     count += 1;
     let resultRow = document.createElement('tr');
-    resultRow.setAttribute('class', 'resultRow');
-
     let radioFrame = document.createElement('td');
     let radioLabel = document.createElement('label');
     let radio = document.createElement('input');
+    let indexFrame = document.createElement('td');
+    let index = document.createElement('p');
+    let resultManufacturer = document.createElement('td');
+    let resultType = document.createElement('td');
+    let resultMinPrice = document.createElement('td');
+    let resultPrice = document.createElement('td');
+    let deleteFrame = document.createElement('td');
+    let deleteBtn = document.createElement('button');
+
+    resultRow.setAttribute('class', 'resultRow');
     radio.setAttribute('type', 'radio');
     radio.setAttribute('name', 'radioOption');
     radio.setAttribute('class', 'radioBtn');
     radio.onclick = function () {
-      
       let row = radio.parentElement.parentElement.parentElement;
 
-      document.getElementById('manufacturerInput').value = row.childNodes[2].value;
+      document.getElementById('manufacturerInput').value =
+        row.childNodes[2].value;
       document.getElementById('typeInput').value = row.childNodes[3].value;
       document.getElementById('priceInput').value = row.childNodes[4].value;
       document.getElementById('minPriceInput').value = row.childNodes[5].value;
     };
     resultRow.appendChild(radioFrame).appendChild(radioLabel).appendChild(radio);
-
-    let indexFrame = document.createElement('td');
-    let index = document.createElement('p');
     index.setAttribute('class', 'index');
     index.innerHTML = count;
     resultRow.appendChild(indexFrame).appendChild(index);
 
-    let resultManufacturer = document.createElement('td');
     resultManufacturer.value = manufacturer;
     resultManufacturer.innerHTML = manufacturer;
     resultManufacturer.setAttribute('class', 'resultManufacturer');
     resultRow.appendChild(resultManufacturer);
 
-    let resultType = document.createElement('td');
     resultType.value = type;
     resultType.innerHTML = type;
     resultType.setAttribute('class', 'resultType');
     resultRow.appendChild(resultType);
 
-    let resultMinPrice = document.createElement('td');
     resultMinPrice.value = minPrice;
     resultMinPrice.innerHTML = minPrice;
     resultMinPrice.setAttribute('class', 'resultMinPrice');
     resultRow.appendChild(resultMinPrice);
 
-    let resultPrice = document.createElement('td');
     resultPrice.value = price;
     resultPrice.innerHTML = price;
     resultPrice.setAttribute('class', 'resultPrice');
     resultRow.appendChild(resultPrice);
 
-    let deleteFrame = document.createElement('td');
-    let deleteBtn = document.createElement('button');
     deleteBtn.setAttribute('id', 'deleteBtn');
     resultRow.appendChild(deleteFrame).appendChild(deleteBtn);
     deleteBtn.onclick = function () {
@@ -69,7 +68,6 @@ document.getElementById('btnInsert').addEventListener('click', function () {
     };
 
     deleteBtn.innerHTML = '刪除';
-
     resultTable.appendChild(resultRow);
   }
 });
@@ -95,42 +93,21 @@ document.getElementById('btnUpdate').addEventListener('click', function () {
   let type = document.getElementById('typeInput').value;
   let minPrice = document.getElementById('minPriceInput').value;
   let price = document.getElementById('priceInput').value;
+  let rows = document.getElementsByClassName('resultRow');
+  let selectedRow = document.querySelector('input[name="radioOption"]:checked')
+    .parentElement.parentElement;
 
   if (document.querySelector('input[name="radioOption"]:checked') === null) {
     alert('請選擇欲修改的欄位');
     return;
-  } else if (!manufacturer.trim() || !type.trim()) {
+  }
+  if (!manufacturer.trim() || !type.trim()) {
     alert('請輸入製造商和類別!');
     return;
   }
-  let rows = document.getElementsByClassName('resultRow');
-  for (let i = 0; i < rows.length; i++) {
-    if (rows[i].querySelector('.radioBtn').checked) {
-      let newManufacturer = document.createElement('td');
-      newManufacturer.value = manufacturer;
-      newManufacturer.innerHTML = manufacturer;
-      rows[i].replaceChild(newManufacturer, rows[i].childNodes[2]);
-  
 
-      let newType = document.createElement('td');
-      newType.value = type;
-      newType.innerHTML = type;
-      rows[i].replaceChild(newType, rows[i].childNodes[3]);
-
-
-      let newMinPrice = document.createElement('td');
-      newMinPrice.value = minPrice;
-      newMinPrice.innerHTML = minPrice;
-      rows[i].replaceChild(newMinPrice, rows[i].childNodes[4]);
- 
-
-      let newPrice = document.createElement('td');
-      newPrice.value = price;
-      newPrice.innerHTML = price;
-      rows[i].replaceChild(newPrice, rows[i].childNodes[5]);
-
-    } else {
-      continue;
-    }
-  }
+  selectedRow.childNodes[2].innerHTML = manufacturer;
+  selectedRow.childNodes[3].innerHTML = type;
+  selectedRow.childNodes[4].innerHTML = minPrice;
+  selectedRow.childNodes[5].innerHTML = price;
 });
